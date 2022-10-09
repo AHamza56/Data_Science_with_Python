57a4e3a2f9ca1599737dc8d7a96607d7b34dec5e
Ali Hamza <109821595+AHamza56@users.noreply.github.com>
2022-10-09 21:25:47 +0500

    Add files via upload

---
+++ 01_Basics_of_Visual_Studio_Code/01_first_program.py
@@ -0,0 +1,7 @@
+print("we are learning python with ammar")
+print(56+89)
+print("Hello World")
+print("How beautiful is the world")
+print("I am learning python")
+print("I am learning python with baba ammar")
+
